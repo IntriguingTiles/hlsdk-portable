d16885c9c77142ae4b52a606f5bdef879b93ec1e
IntriguingTiles <jnickw@outlook.com>
2026-03-15 13:53:04 -0600

    use global reent for rpls

--- dlls/wiiu_rpl_entry_stub.cpp
+++ dlls/wiiu_rpl_entry_stub.cpp
@@ -1,3 +1,7 @@
 #include <coreinit/dynload.h>
 
 extern "C" int rpl_entry(OSDynLoad_Module module, OSDynLoad_EntryReason reason){return 0;}
+extern "C" struct _reent *__wut_getreent(void)
+{
+   return _GLOBAL_REENT;
+}
